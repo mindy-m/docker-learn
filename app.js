5f2e57adc25b0600c70b6749733b05b7dfe2daaf
Mindy Medeiros <mmedei6@wgu.edu>
2024-02-03 00:55:14 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const http = require('node:http');
+
+let hitCount = 0;
+
+// Create a local server to receive data from
+const server = http.createServer((req, res) => {
+  hitCount++;    
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify({
+    data: `${hitCount} goat${hitCount === 1 ? "" : "s"} ${hitCount === 1 ? "has" : "have"} visited this page.`,
+  }));
+  console.log(`hitCount is currently ${hitCount}.`);
+});
+
+server.listen(8080);
