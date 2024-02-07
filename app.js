39d70d991146311a8ac52b901bc6629ad4386665
Mindy Medeiros <mmedei6@wgu.edu>
2024-02-07 00:49:01 -0500

    Now logging request URL to console

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const http = require('node:http');
 let hitCount = 0;
 
 // Create a local server to receive data from
-const server = http.createServer((req, res) => {
+const server = http.createServer((request, response) => {
+  console.log("Look at this frickin' request URL! ->", request.url)
+
   hitCount++;    
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify({
+  response.writeHead(200, { 'Content-Type': 'application/json' });
+  response.end(JSON.stringify({
     data: `${hitCount} goat${hitCount === 1 ? "" : "s"} ${hitCount === 1 ? "has" : "have"} visited this page.`,
   }));
   console.log(`hitCount is currently ${hitCount}.`);
